361691cd883fa7974b332038997a2baf2f95f9e0
gottagitgoing <ruo.cxie@gmail.com>
2020-05-17 23:51:07 -0400

    Add files via upload

---
+++ model_parameters.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+import torch
+import torchvision as tvn
+
+class VNN:
+    def __init__(self, network):
+        self.parameters = network.named_parameters()
+
+class CNN:
+
+
+class RNN:
+
+
+class LSTM:
+
+
+class GAN:
+
