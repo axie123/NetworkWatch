0faa0318c28679bbbb58d034ddeb734286466fb1
gottagitgoing <ruo.cxie@gmail.com>
2020-06-03 18:35:33 -0400

    Update and rename model_parameters.py to pytorch_model_parameters.py

--- pytorch_model_parameters.py
+++ pytorch_model_parameters.py
@@ -1,8 +1,4 @@
 import numpy as np
-import pandas as pd
-import torch
-import torchvision as tvn
-
 
 class VNN:
     def __init__(self, network):
@@ -26,38 +22,38 @@ class VNN:
 
     def weights_to_node(self, layer, node):
         # Gets all the weights coming into the node.
-        hidden_layer = self.weights[layer]
+        hidden_layer = self.weights.get(layer)
         node_weights = hidden_layer[node - 1]
         return node_weights
 
-    def node_to_node_weight(self, start_node, end_layer, end_node):
+    def node_to_node_weight(self, start_node, end_node, end_layer):
         # Weight going between two nodes.
-        e_layer = self.weights[end_layer]
+        e_layer = self.weights.get(end_layer)
         n_to_n_weight = e_layer[end_node - 1][start_node - 1]
         return n_to_n_weight
 
     def weight_range(self, layer, node, start_weight, end_weight):
         # Get a range of nodes coming into it.
-        return self.weights[layer][node - 1][start_weight - 1:end_weight]
+        return self.weights.get(layer)[node - 1][start_weight - 1:end_weight]
 
     def bias_of_node(self, layer, node):
         # Get a node bias.
-        hidden_layer = self.bias[layer]
+        hidden_layer = self.bias.get(layer)
         node_bias = hidden_layer[node - 1]
         return node_bias
 
-    def bias_range(self, layer,start_node, end_node):
+    def bias_range(self, layer,  start_node, end_node):
         # Get a range of node biases.
-        return self.bias[layer][start_node - 1:end_node]
+        return self.bias.get(layer)[start_node - 1:end_node]
 
 
-class CNN:
+class CNN(VNN):
     def __init__(self, network):
         # Organizes all the parameters of the CNN.
         #
         # Separates the name of the parameter from parameter.
-        self.parameters = np.array(list(network.named_parameters()))
-        self.keys = self.parameters[:, 0]
+        #self.parameters = np.array(list(network.named_parameters()))
+        #self.keys = self.parameters[:, 0]
         self.kernels_weights_biases = self.parameters[:, 1]
 
         self.bias = {}  # Biases
@@ -75,53 +71,54 @@ class CNN:
         self.kernels_key = self.kernels.keys()
         self.weights_key = self.weights.keys()
 
-    def kernel_filter(self, conv_layer, prev_filter_num, filter_group_num):
+    def kernel_filter(self, conv_layer, filter_group_num, prev_filter_num):
         # View the kernel filter.
-        filter = self.kernels[conv_layer][filter_group_num-1]\
+        filter = self.kernels.get(conv_layer)[filter_group_num-1]\
         [prev_filter_num-1]
         return filter
 
-    def kernel_filter_range(self, conv_layer, prev_filter_num_start,
-                            prev_filter_num_end, filter_group_num):
+    def kernel_filter_range(self, conv_layer, filter_group_num, 
+                            prev_filter_num_start,
+                            prev_filter_num_end):
         # Gives a range of kernel filters.
-        filter_range = self.kernels[conv_layer][filter_group_num-1]\
+        filter_range = self.kernels.get(conv_layer)[filter_group_num-1]\
         [prev_filter_num_start-1:prev_filter_num_end]
         return filter_range
 
     def fc_weights_to_node(self, layer, node):
         # Gets all the weights coming into the node.
-        hidden_layer = self.weights[layer]
+        hidden_layer = self.weights.get(layer)
         node_weights = hidden_layer[node - 1]
         return node_weights
 
-    def fc_node_to_node_weight(self, start_node, end_layer, end_node):
+    def fc_node_to_node_weight(self, start_node, end_node, end_layer):
         # Weight going between two nodes.
-        e_layer = self.weights[end_layer]
+        e_layer = self.weights.get(end_layer)
         n_to_n_weight = e_layer[end_node - 1][start_node - 1]
         return n_to_n_weight
 
     def fc_weight_range(self, layer, node, start_weight, end_weight):
         # Get a range of nodes coming into it.
-        return self.weights[layer][node - 1][start_weight - 1:end_weight]
+        return self.weights.get(layer)[node - 1][start_weight - 1:end_weight]
 
     def fc_bias_of_node(self, layer, node):
         # Get a node bias.
-        hidden_layer = self.bias[layer]
+        hidden_layer = self.bias.get(layer)
         node_bias = hidden_layer[node - 1]
         return node_bias
 
     def fc_bias_range(self, layer, start_node, end_node):
         # Get a range of node biases.
-        return self.bias[layer][start_node - 1:end_node]
+        return self.bias.get(layer)[start_node - 1:end_node]
 
 # I don't know much about the GRU, so this is just a skeleton so far.
-class GRU:
+class GRU(VNN):
     def __init__(self, network):
         # Organizes all the parameters of the GRU.
         #
         # Separates the name of the parameter from parameter.
-        self.parameters = np.array(list(network.named_parameters()))
-        self.keys = self.parameters[:, 0]
+        #self.parameters = np.array(list(network.named_parameters()))
+        #self.keys = self.parameters[:, 0]
         self.gru_paras_embedding = self.parameters[:, 1]
 
         self.bias = {}  # Biases
@@ -141,5 +138,21 @@ class GRU:
         self.bias_key = self.bias.keys()
         self.gru_ih_key = self.gru_ih.keys()
         self.gru_hh_key = self.gru_hh.keys()
+
+class output_visualizer:
+    def __init__(self, x, w, b):
+        self.x = x
+        self.w = w
+        self.b = b
+
+    def layer_output(self, activation):
+        z = activation(np.matmul(np.transpose(self.w), self.x) + self.b)
+        return z
+    
+
+
+
+
+
 
 
